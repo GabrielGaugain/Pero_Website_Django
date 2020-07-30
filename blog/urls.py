df7fcc2d5e2ad58c45631331d996a97be9bfbfd0
Gaugain <gabrielthahand@gmail.com>
2020-07-30 14:59:33 +0200

    ajout article dans blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('blog/', views.index_blog, name='index_blog' ),
+    path('index', views.index_blog, name='index_blog' ),
+    path('',views.accueil, name='accueil'),
+    path('article/<int:id>', views.lire, name='lire')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
+from blog.models import Article
 
 def index_blog(request):
     return render(request, 'blog/index_blog.html')
+
+
+def accueil(request):
+    """ Afficher tous les articles de notre blog """
+    articles = Article.objects.all() # Nous sélectionnons tous nos articles
+    return render(request, 'blog/accueil.html', {'derniers_articles': articles})
+
+def lire(request, id):
+    """ Afficher un article complet """
+    pass # Le code de cette fonction est donné un peu plus loin.
